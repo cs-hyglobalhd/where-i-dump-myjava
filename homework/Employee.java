4789cf54a0a1efc05d9aadcdd66496ba45eb0c7d
HyGlobalHD <GamerXzavier@gmail.com>
2020-03-12 10:37:41 +0800

    Update Employee.java

--- homework/Employee.java
+++ homework/Employee.java
@@ -1,3 +1,25 @@
 public class Employee {
+   private String employeeName;
+   private int employeeID;
+   private double employeeSalary;
 
+ 
+   public void input(String name, int id, double salary) {
+     employeeName = name;
+     employeeID = id;
+     employeeSalary = salary;
+
+   }
+  
+   public double calcYearlySalary() {
+     return employeeSalary * 12;
+   }
+  
+   public void display(){
+     System.out.println("Employee Name: " + employeeName);
+     System.out.println("Employee ID: " + employeeID);
+     System.out.println("Salary per month: " + employeeSalary);
+     System.out.println("Hours Worked: " + hoursWorked);
+   }
+  
 }
